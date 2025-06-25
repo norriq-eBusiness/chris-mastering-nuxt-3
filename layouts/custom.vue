e303cc388c917066311f3ff18f936c8f318dc04a
Christian Valentin <christian.valentin@norriq.com>
2025-06-25 15:38:07 +0200

    Added localestorage

---
+++ layouts/custom.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="p-12 bg-blue-500 w-full h-full min-h-screen flex flex-col items-center"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
--- pages/course.vue
+++ pages/course.vue
@@ -3,48 +3,44 @@ const { chapters } = useCourse();
 </script>
 
 <template>
-  <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
-  >
-    <div class="prose mb-12">
-      <h1>
-        <span class="font-medium">
-          Course:
-          <span class="font-bold">Mastering Nuxt 3</span>
-        </span>
-      </h1>
+  <div class="prose mb-12">
+    <h1>
+      <span class="font-medium">
+        Course:
+        <span class="font-bold">Mastering Nuxt 3</span>
+      </span>
+    </h1>
+  </div>
+
+  <div class="flex justify-center w-full max-w-[1500px]">
+    <div
+      class="prose mr-4 p-8 bg-white rounded-md w-fit min-w-[20%] flex flex-col"
+    >
+      <h3>Chapters</h3>
+      <div
+        class="space-y-1 mb-4 flex flex-col"
+        v-for="chapter in chapters"
+        :key="chapter.slug"
+      >
+        <h4>{{ chapter.title }}</h4>
+        <NuxtLink
+          v-for="(lesson, index) in chapter.lessons"
+          :to="lesson.path"
+          :key="lesson.slug"
+          class="flex space-x-1 no-underline prose-sm"
+          :class="
+            lesson.path === $route.fullPath
+              ? 'text-orange-600'
+              : 'text-gray-600'
+          "
+        >
+          <span>{{ index + 1 }}. {{ lesson.title }}</span>
+        </NuxtLink>
+      </div>
     </div>
 
-    <div class="flex justify-center w-full max-w-[1500px]">
-      <div
-        class="prose mr-4 p-8 bg-white rounded-md w-fit min-w-[20%] flex flex-col"
-      >
-        <h3>Chapters</h3>
-        <div
-          class="space-y-1 mb-4 flex flex-col"
-          v-for="chapter in chapters"
-          :key="chapter.slug"
-        >
-          <h4>{{ chapter.title }}</h4>
-          <NuxtLink
-            v-for="(lesson, index) in chapter.lessons"
-            :to="lesson.path"
-            :key="lesson.slug"
-            class="flex space-x-1 no-underline prose-sm"
-            :class="
-              lesson.path === $route.fullPath
-                ? 'text-orange-600'
-                : 'text-gray-600'
-            "
-          >
-            <span>{{ index + 1 }}. {{ lesson.title }}</span>
-          </NuxtLink>
-        </div>
-      </div>
-
-      <div class="p-12 bg-white rounded-md w-full">
-        <NuxtPage />
-      </div>
+    <div class="p-12 bg-white rounded-md w-full">
+      <NuxtPage />
     </div>
   </div>
 </template>
